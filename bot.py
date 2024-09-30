d97c3a9c9da1494d3654e4bfc1c97eea3419496c
hxxler <113106251+hxxler@users.noreply.github.com>
2024-09-30 22:09:25 +0300

    Updated to use image for clicking

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 
-TOKEN = 'твой_токен_здесь'
+TOKEN = '7879104456:AAHIzi9XN5Pfd1Wef_GG9Pk32ZKcZ3vbN3Uзн'
 bot = telebot.TeleBot(TOKEN)
 
 # Обработчик команды /start
@@ -9,7 +9,7 @@ def send_welcome(message):
     bot.reply_to(message, "Привет! Добро пожаловать в кликер-игру! Жми на кнопку ниже, чтобы играть.")
     
     # Ссылка на веб-страницу с кликером
-    bot.send_message(message.chat.id, "Играй тут: [Clicker Game](https://твой_домен)", parse_mode="Markdown")
+    bot.send_message(message.chat.id, "Играй тут: [Clicker Game](https://elegant-syrniki-0fb98a.netlify.app/)", parse_mode="Markdown")
 
 # Запуск бота
 bot.polling()
